0028da9b164cff0e14a3fd513bf2ec93b607311d
Carlos Vinicius <vinicarlos56@hotmail.com>
2014-11-27 15:28:30 -0200

    added powerline fonts to the repo and a install script, wrong submodule setup fixed

---
+++ install.sh
@@ -0,0 +1,9 @@
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
+
+git submodule init
+
+git submodule update
+
+mkdir $HOME/.fonts/
+
+cp powerline-fonts/UbuntuMono/Ubuntu\ Mono\ derivative\ Powerline.ttf $HOME/.fonts/
